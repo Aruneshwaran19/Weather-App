1fc30a4110013004c37ee4f8a3c5142c2c7d8296
Aruneshwaran <aruneshwaran.v19@gmail.com>
2024-12-20 20:25:37 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const searchBtn = document.getElementById('searchBtn');
 const cityInput = document.getElementById('city');
 const weatherDetails = document.getElementById('weatherDetails');
 
-const apiKey = "YOUR_API_KEY"; // Replace with your OpenWeatherMap API key
+const apiKey = process.env.API_KEY;; // Replace with your OpenWeatherMap API key
 
 searchBtn.addEventListener('click', fetchWeather);
 
